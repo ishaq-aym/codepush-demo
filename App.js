745338e8f34582607bf9c37d55c4bbb026ec7a0b
Ishaq Ashraf <ishaq@aymcommerce.com>
2019-03-03 11:30:16 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,62 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ */
+
+import React, {Component} from 'react';
+import {StyleSheet, Text, View,Button} from 'react-native';
+import CodePush from 'react-native-code-push';
+
+
+const CodePushConfig = {
+  installMode: CodePush.CheckFrequency.MANUAL
+}
+
+
+class App extends Component {
+
+  checkForUpdates = () => {
+    CodePush.sync({
+      updateDialog: false,
+      installMode: CodePush.InstallMode.IMMEDIATE
+    })
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to Code Push v2!</Text>
+        <Button
+          title='Check for Updates'
+          onPress={this.checkForUpdates}
+        />     
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+
+export default CodePush(CodePushConfig)(App);
